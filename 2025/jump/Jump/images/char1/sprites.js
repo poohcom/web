9bda4b4de8b8585e736861b2e7168638406a36ee
poohcom <poohcom@naver.com>
2025-11-21 15:45:39 +0900

    add

---
+++ 2025/jump/Jump/images/char1/sprites.js
@@ -0,0 +1,75 @@
+{
+"frames": {
+	"Dead00.png": {
+		"frame": {"x":0, "y":406, "w":172, "h":202},
+		"spriteSourceSize": {"x":33,"y":12,"w":244,"h":244},
+		"sourceSize": {"w":244,"h":244}
+	},
+	"Dead01.png": {
+		"frame": {"x":496, "y":397, "w":149, "h":212},
+		"spriteSourceSize": {"x":49,"y":18,"w":244,"h":244},
+		"sourceSize": {"w":244,"h":244}
+	},
+	"Dead02.png": {
+		"frame": {"x":340, "y":207, "w":155, "h":206},
+		"spriteSourceSize": {"x":46,"y":21,"w":244,"h":244},
+		"sourceSize": {"w":244,"h":244}
+	},
+	"Dead03.png": {
+		"frame": {"x":188, "y":0, "w":164, "h":206},
+		"spriteSourceSize": {"x":43,"y":22,"w":244,"h":244},
+		"sourceSize": {"w":244,"h":244}
+	},
+	"Dead04.png": {
+		"frame": {"x":340, "y":414, "w":155, "h":212},
+		"spriteSourceSize": {"x":46,"y":18,"w":244,"h":244},
+		"sourceSize": {"w":244,"h":244}
+	},
+	"Idle01.png": {
+		"frame": {"x":508, "y":0, "w":148, "h":199},
+		"spriteSourceSize": {"x":48,"y":24,"w":244,"h":244},
+		"sourceSize": {"w":244,"h":244}
+	},
+	"Idle02.png": {
+		"frame": {"x":496, "y":200, "w":154, "h":196},
+		"spriteSourceSize": {"x":45,"y":27,"w":244,"h":244},
+		"sourceSize": {"w":244,"h":244}
+	},
+	"Idle03.png": {
+		"frame": {"x":176, "y":207, "w":163, "h":196},
+		"spriteSourceSize": {"x":42,"y":27,"w":244,"h":244},
+		"sourceSize": {"w":244,"h":244}
+	},
+	"Idle04.png": {
+		"frame": {"x":353, "y":0, "w":154, "h":199},
+		"spriteSourceSize": {"x":45,"y":24,"w":244,"h":244},
+		"sourceSize": {"w":244,"h":244}
+	},
+	"Jump01.png": {
+		"frame": {"x":508, "y":0, "w":148, "h":199},
+		"spriteSourceSize": {"x":48,"y":24,"w":244,"h":244},
+		"sourceSize": {"w":244,"h":244}
+	},
+	"Jump02.png": {
+		"frame": {"x":0, "y":0, "w":187, "h":205},
+		"spriteSourceSize": {"x":33,"y":12,"w":244,"h":244},
+		"sourceSize": {"w":244,"h":244}
+	},
+	"Jump03.png": {
+		"frame": {"x":0, "y":206, "w":175, "h":199},
+		"spriteSourceSize": {"x":36,"y":21,"w":244,"h":244},
+		"sourceSize": {"w":244,"h":244}
+	},
+	"Jump04.png": {
+		"frame": {"x":173, "y":406, "w":166, "h":193},
+		"spriteSourceSize": {"x":42,"y":30,"w":244,"h":244},
+		"sourceSize": {"w":244,"h":244}
+	}
+
+},
+"meta": {
+	"image": "sprites.png",
+	"size": {"w": 657, "h": 627},
+	"scale": "1"
+}
+}
