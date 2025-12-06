f80d987b8b056e57bf4ab27f54b5e086080299e4
poohcom <poohcom1004@gmail.com>
2025-12-06 14:54:26 +0900

    sfff

---
+++ 2025_jump/images/effect/sprites.js
@@ -0,0 +1,65 @@
+{
+"frames": {
+	"line00.png": {
+		"frame": {"x":292, "y":0, "w":68, "h":104},
+		"spriteSourceSize": {"x":162,"y":256,"w":400,"h":360},
+		"sourceSize": {"w":400,"h":360}
+	},
+	"line01.png": {
+		"frame": {"x":505, "y":302, "w":78, "h":190},
+		"spriteSourceSize": {"x":152,"y":170,"w":400,"h":360},
+		"sourceSize": {"w":400,"h":360}
+	},
+	"line02.png": {
+		"frame": {"x":426, "y":302, "w":78, "h":264},
+		"spriteSourceSize": {"x":152,"y":50,"w":400,"h":360},
+		"sourceSize": {"w":400,"h":360}
+	},
+	"line03.png": {
+		"frame": {"x":347, "y":375, "w":78, "h":184},
+		"spriteSourceSize": {"x":152,"y":0,"w":400,"h":360},
+		"sourceSize": {"w":400,"h":360}
+	},
+	"line04.png": {
+		"frame": {"x":286, "y":302, "w":78, "h":72},
+		"spriteSourceSize": {"x":152,"y":0,"w":400,"h":360},
+		"sourceSize": {"w":400,"h":360}
+	},
+	"water01.png": {
+		"frame": {"x":220, "y":447, "w":126, "h":78},
+		"spriteSourceSize": {"x":87,"y":192,"w":300,"h":270},
+		"sourceSize": {"w":300,"h":270}
+	},
+	"water02.png": {
+		"frame": {"x":0, "y":447, "w":219, "h":141},
+		"spriteSourceSize": {"x":39,"y":129,"w":300,"h":270},
+		"sourceSize": {"w":300,"h":270}
+	},
+	"water03.png": {
+		"frame": {"x":0, "y":239, "w":285, "h":207},
+		"spriteSourceSize": {"x":3,"y":63,"w":300,"h":270},
+		"sourceSize": {"w":300,"h":270}
+	},
+	"water04.png": {
+		"frame": {"x":286, "y":127, "w":267, "h":174},
+		"spriteSourceSize": {"x":9,"y":96,"w":300,"h":270},
+		"sourceSize": {"w":300,"h":270}
+	},
+	"water05.png": {
+		"frame": {"x":0, "y":0, "w":291, "h":126},
+		"spriteSourceSize": {"x":3,"y":144,"w":300,"h":270},
+		"sourceSize": {"w":300,"h":270}
+	},
+	"water06.png": {
+		"frame": {"x":0, "y":127, "w":285, "h":111},
+		"spriteSourceSize": {"x":6,"y":159,"w":300,"h":270},
+		"sourceSize": {"w":300,"h":270}
+	}
+
+},
+"meta": {
+	"image": "sprites.png",
+	"size": {"w": 584, "h": 589},
+	"scale": "1"
+}
+}
