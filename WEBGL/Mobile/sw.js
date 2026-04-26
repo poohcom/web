841f1044bbd514f0eefcefc69f2089640ea160e2
poohcom <poohcom@naver.com>
2026-04-27 00:30:19 +0900

    Unity 빌드 파일 LFS 적용

--- WEBGL/Mobile/sw.js
+++
@@ -1,7 +0,0 @@
-// sw.js — no caching, just for install banner support
-self.addEventListener('install', event => {
-  self.skipWaiting(); // 바로 활성화
-});
-self.addEventListener('activate', event => {
-  // 아무 작업도 하지 않음
-});
